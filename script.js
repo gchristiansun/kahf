00a3976eb0ede572bdc7b5d337ac50b78a120562
gchristiansun <167200722+gchristiansun@users.noreply.github.com>
2024-07-26 05:55:33 +0700

    revisi2

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 window.addEventListener('scroll', function() {
-    var overlayImage = document.querySelector('overlayImage');
-    if (window.scrollY > 0) {
+    var overlayImage = document.querySelector('.overlayImage');
+    if (window.scrollY > 50) {
         overlayImage.classList.add('hidden');
     } else {
         overlayImage.classList.remove('hidden');
     }
+    });
+    
+window.addEventListener('DOMContentLoaded', (event) => {
+    const overlay = document.getElementById('overlay_image')
+    if (window.innerWidth < 950) {
+        overlay.style.display = 'none'
+    } 
     });
     
     
